8a103c4249efb87efa277e106a959059604f9b92
guttweiler-hash <guttweiler@gmail.com>
2025-10-01 17:33:40 -0400

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// Simple form handler
-document.getElementById("mini-form").addEventListener("submit", function(e) {
-  e.preventDefault();
-  document.getElementById("mini-msg").innerText = "✅ Thanks! We'll contact you shortly.";
-  this.reset();
-});
